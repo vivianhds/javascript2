e00064384f6c36f39d2764199e36dba3e82873f2
vivianhds <vvnhenriquee@gmail.com>
2018-08-23 20:22:42 -0300

    javascript2

---
+++ aula2/js/funcoes.js
@@ -0,0 +1,12 @@
+function trocaImagem() {
+	document.getElementById("myImage").src="img/html-logo-bagde-512.png";
+}
+function imagemTroca() {
+	document.getElementById("myImage").src="img/js-logo-badge.png";
+}
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="35px"
+}
+function Fontetroca() {
+	document.getElementById("demo").style.fontSize="18px"
+}
