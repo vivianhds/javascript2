23983ea06f5d524cc71a64a2328b9e60a52de558
vivianhds <vvnhenriquee@gmail.com>
2018-08-23 21:50:13 -0300

    javascript

---
+++ aula2/Nova pasta/js/funcoes.js
@@ -0,0 +1,16 @@
+
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="35px"
+}
+function Fontetroca() {
+	document.getElementById("demo").style.fontSize="18px"
+}
+function ocultarElemento(){
+	document.getElementById("demo").style.display="none";
+}	
+function mostrarElemento(){
+	document.getElementById("demo").style.display="block";
+}	
+function calcular(){
+	document.getElementById("demo").innerHTML= 10 + 5;
+}	
